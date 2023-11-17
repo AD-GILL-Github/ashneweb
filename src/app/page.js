650c9024342f6ee0c2a9c87811d187babc88c1f2
AD GILL <adgill.127.0.0.1@gmail.com>
2023-11-17 16:03:56 +0530

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,7 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
-import AboutSection from "./components/AboutSection";
-import ProjectsSection from "./components/ProjectsSection";
-import EmailSection from "./components/EmailSection";
-import Footer from "./components/Footer";
-import AchievementsSection from "./components/AchievementsSection";
+
 
 export default function Home() {
   return (
